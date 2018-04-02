159ba1b576a10830cee2eb6ac67da6c52f6ecb14
wangruozhou <wangruozhouop@gmail.com>
2018-04-03 00:18:12 +0800

    first commit

---
+++ src/Demo1.java
@@ -0,0 +1,8 @@
+public class Demo1 {
+    public static void print(int i,Object obj){
+        System.out.println(i + ", "+obj.toString());
+    }
+    public static void main(String[] args){
+        print(1,"hello");
+    }
+}
